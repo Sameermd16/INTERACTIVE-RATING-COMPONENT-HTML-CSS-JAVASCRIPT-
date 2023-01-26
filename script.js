1ca78932536320bfb9ce69ab479257cd8409a2c3
Sameer Md <sameermd@Sameers-MacBook-Pro.local>
2023-01-26 15:46:19 +0530

    JavaScript

---
+++ script.js
@@ -0,0 +1,14 @@
+const box1 = document.querySelector(".box-1");
+const box2 = document.querySelector(".box-2");
+const submitButton = document.querySelector(".submit-button");
+const rateAgain = document.querySelector(".rate-again");
+
+
+submitButton.addEventListener("click", () => {
+    box2.classList.remove("hidden");
+    box1.style.display = "none";
+})
+rateAgain.addEventListener("click", () => {
+    box2.classList.add("hidden");
+    box11.style.display = "block";
+})
