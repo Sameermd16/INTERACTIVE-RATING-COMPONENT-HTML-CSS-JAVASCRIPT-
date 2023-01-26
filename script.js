7e0e1eba05996cdb0700666169b2fe782b6151e7
Sameer Md <sameermd@Sameers-MacBook-Pro.local>
2023-01-27 03:32:07 +0530

    JavaScript

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const box1 = document.querySelector(".box-1");
 const box2 = document.querySelector(".box-2");
 const submitButton = document.querySelector(".submit-button");
 const rateAgain = document.querySelector(".rate-again");
-
+const buttons = document.querySelectorAll(".btn");
+const rating = document.getElementById("rating");
 
 submitButton.addEventListener("click", () => {
     box2.classList.remove("hidden");
@@ -10,5 +11,15 @@ submitButton.addEventListener("click", () => {
 })
 rateAgain.addEventListener("click", () => {
     box2.classList.add("hidden");
-    box11.style.display = "block";
+    box1.style.display = "block";
 })
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        rating.innerHTML = button.innerHTML;
+    })
+})
+
+// buttons.addEventListener("click", () => {
+//     rating.innerHTML = button.innerHTML;
+// })
